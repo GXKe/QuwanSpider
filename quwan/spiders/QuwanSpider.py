aecfb207094b6ab596855d8abbe7d7c4df04141b
Gh72 <cao.wang.work@foxmail.com>
2017-02-21 14:51:11 +0800

    首页列表及产品详情抓取完成

--- quwan/spiders/QuwanSpider.py
+++ quwan/spiders/QuwanSpider.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapy.selector import Selector
 from scrapy.http import Request
+import re
 import time
 
 
@@ -31,36 +32,69 @@ class QuwanspiderSpider(scrapy.Spider):
             site = "http://www.quwan.com/"
             if url.find(site) == -1:
                 url = site + url
-            print("生成抓取任务：" + url)
-            yield Request(url=url, callback=self.parse_details, meta={'url': url})
-        print("抓取产品列表总数：%d" % count)
+
+            urlid = re.compile(r'//.*/(.*?)\.html').findall(url)[0]
+            print("=====生成抓取任务："+ urlid + url)
+            yield Request(url=url, callback=self.parse_details, meta={'url': url, 'urlid':urlid})
+        print("=====抓取产品列表总数：%d" % count)
 
 
     def parse_details(self, response):
         self.item+=1;
         print("item idx: %d" % self.item)
-        print("抓取到详情页url：" + response.meta['url'])
-        #time.sleep(1)
+        print("=====抓取到详情页url：" + response.meta['url'])
+        print("=====商品id：" + response.meta['urlid'])
+
 
         #解析产品详情
         sel = Selector(response)
 
-        print("缩略大图1  href")
+        print("=====缩略大图1  href")
         imgs = sel.xpath('//a[@class ="cloud-zoom"]/@href').extract()
         for img in imgs:
             print(img)
 
-        print("缩略大图2 src")
+        print("=====缩略大图2 src")
         imgs = sel.xpath('//a[@class ="cloud-zoom"]/img/@src').extract()
         for img in imgs:
             print(img)
 
-        print("缩略小图 src")
+        print("=====缩略小图 src")
         imgs = sel.xpath('//ul[@class="pic_index"]/li[@class="pic_li"]/img/@src').extract()
         for img in imgs:
             print(img)
 
-        print("资源详情图")
+        print("=====资源详情图")
         imgs = sel.xpath('//div[@class="box details"]/p/img/@src').extract()
+        for img in imgs:
+            print(img)
+
+        print("=====商品介绍")
+        imgs = sel.xpath('//div[@class="gn_decri"]/p/text()').extract()
+        for img in imgs:
+            print(img)
+        print("=====商品参数名")
+        imgs = sel.xpath('//ul[@class="csList"]/li/b/text()').extract()
+        for img in imgs:
+            print(img)
+
+        print("=====商品参数值")
+        imgs = sel.xpath('//ul[@class="csList"]/li/text()').extract()
+        for img in imgs:
+            print(img)
+
+        print("=====品牌")
+        imgs = sel.xpath('//div[@id="paykey_new"]//ul/li/dl/dd/a/text()').extract()
+        #for img in imgs:
+         #   print(imgs)
+        print(imgs[0])
+
+        print("=====价格")
+        imgs = sel.xpath('//dt[@id="price_goods_div"]/strong/text()').extract()
+        for img in imgs:
+            print(img)
+
+        print("=====名称")
+        imgs = sel.xpath('//div[@class="infor"]/h1/text()').extract()
         for img in imgs:
             print(img)
